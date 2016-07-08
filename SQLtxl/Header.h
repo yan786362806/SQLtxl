58032e53e8c7deec35cd199df0d16fc020c78139
yan786362806 <786362806@qq.com>
2016-07-08 12:00:17 +0800

    txl
    这节课彻底搞明白了创建数据库，添加到自己的程序中，上节课做的都错了，彻底重做，一个页面对应一个代码，添加，删除，查找，重新做，还可以继续添加更多
    的功能，继续完善

---
+++ SQLtxl/Header.h
@@ -0,0 +1,10 @@
+//
+//  Header.h
+//  SQLtxl
+//
+//  Created by 20141105044y on 16/7/8.
+//  Copyright © 2016年 20141105044y. All rights reserved.
+//
+
+#import "sqlite3.h"
+#import <time.h>
---
+++ SQLtxl/delete.swift
@@ -0,0 +1,24 @@
+//
+//  delete.swift
+//  SQLtxl
+//
+//  Created by 20141105044y on 16/7/8.
+//  Copyright © 2016年 20141105044y. All rights reserved.
+//
+
+import UIKit
+
+class delete: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
